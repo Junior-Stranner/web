4b36c9cd6c96469db7b418c1ec267474d7dfada6
Junior-Stranner <JuniorStranner@gmail.com>
2025-07-21 15:55:23 -0300

    exclusão de alguns arquivos

--- Atividade-Heinz-Jr/Atividade3/ProdutosFiltroPorCategoria/ARray.js
+++
@@ -1,7 +0,0 @@
-const products = [
-  { id: 1, nome: "Smartphone", categoria: "eletronicos", preco: 1500 },
-  { id: 2, nome: "Notebook", categoria: "eletronicos", preco: 3500 },
-  { id: 3, nome: "Camisa", categoria: "moda", preco: 80 },
-  { id: 4, nome: "Livro JS", categoria: "livros", preco: 45 },
-  { id: 5, nome: "Tênis", categoria: "moda", preco: 200 },
-];
--- Atividade-Heinz-Jr/Atividade3/ProdutosFiltroPorCategoria/Filtragem.js
+++
@@ -1,9 +0,0 @@
-function filterProducts(category) {
-  const filtered =
-    category === "all"
-      ? products
-      : products.filter((p) => p.categoria === category);
-  renderProducts(filtered);
-}
-
-renderProducts(products);
